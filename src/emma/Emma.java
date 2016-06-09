664e07a1fb2595585b7cfa9ff882c4bbfe8253ba
ps093 <ps093@hdm-stuttgart.de>
2016-06-09 13:01:55 +0200

    Name finder läuft

--- src/emma/Emma.java
+++ src/emma/Emma.java
@@ -8,13 +8,17 @@ package emma;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.sentdetect.SentenceDetector;
 import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
 
 /**
  *
@@ -30,12 +34,12 @@ public class Emma {
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
 
-        String file_name = "C:\\Users\\Patrick\\Documents\\Emma_Chap_I_Jane_Austen.txt";
+        String file_name = "C:\\Users\\Patrick\\Documents\\Emma.txt";
 
-        InputStream sentenceIn = null;
+        InputStream sentencesIn = null;
         InputStream tokenIn = null;
-        InputStream locationIn = null;
-        
+        InputStream personIn = null;
+
         //nur ab hier coden
         try {
             EmmaParser file = new EmmaParser(file_name);
@@ -43,42 +47,39 @@ public class Emma {
 
             int i = 0;
             int j = 0;
-            
-                
-            
-            sentenceIn = new FileInputStream("en-sent.bin");
-            SentenceModel sentence = new SentenceModel(sentenceIn);
+
+            sentencesIn = new FileInputStream("en-sent.bin");
+            SentenceModel sentence = new SentenceModel(sentencesIn);
             SentenceDetectorME sentenceDetector = new SentenceDetectorME(sentence);
             String sentences[] = sentenceDetector.sentDetect(aryLines);
-            
+
             tokenIn = new FileInputStream("en-token.bin");
             TokenizerModel token = new TokenizerModel(tokenIn);
             Tokenizer tokenizer = new TokenizerME(token);
-            
-            locationIn = new FileInputStream("en-ner-location.bin");
-            TokenNameFinderModel location = new TokenNameFinderModel(locationIn);
-            NameFinderME nameFinder = new NameFinderME(location);
-            
+
+            personIn = new FileInputStream("en-ner-person.bin");
+            TokenNameFinderModel person = new TokenNameFinderModel(personIn);
+            NameFinderME nameFinder = new NameFinderME(person);
+
             for (i = 0; i < sentences.length; i++) {
-            String tokens[] = tokenizer.tokenize(sentences[i]);
-            
-            for (j = 0; j < tokens.length; j++) {
-                System.out.println(tokens[j]);
+                String tokens[] = tokenizer.tokenize(sentences[i]);
+
+                Span nameSpans[] = nameFinder.find(tokens);
+                
+                 
+
+                for (j = 0; j < nameSpans.length; j++) {
+                    for (int k = nameSpans[j].getStart(); k < nameSpans[j].getEnd(); k++) {
+                        System.out.println(tokens[k]);
+                    }
                 }
-            
             }
-            
-            
-            
-                    
-            
-      //nur bis hier coden      
+
+            //nur bis hier coden      
         } catch (IOException e) {
             throw new RuntimeException(e);
-            
-            
 
-        } 
+        }
 
     }
 
