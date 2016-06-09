e62730ce8cd404184e460fcda6141b052dfa7668
ps093 <ps093@hdm-stuttgart.de>
2016-06-09 10:46:12 +0200

    Sentenicer funktioniert

--- src/emma/Emma.java
+++ src/emma/Emma.java
@@ -4,11 +4,12 @@
  * and open the template in the editor.
  */
 package emma;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileInputStream;
+import opennlp.tools.sentdetect.SentenceDetectorME;
 import opennlp.tools.sentdetect.SentenceModel;
-
 
 /**
  *
@@ -16,46 +17,53 @@ import opennlp.tools.sentdetect.SentenceModel;
  */
 public class Emma {
 
+    private static SentenceModel model;
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
 
-           String file_name = "C:\\Users\\Patrick\\Documents\\Emma_Chap_I_Jane_Austen.txt";      
-    
-    try {
-    EmmaParser file = new EmmaParser( file_name );
-    String[ ] aryLines = file.OpenFile( );
-    
-        int i;
-        for ( i=0; i < aryLines.length; i++ ) {
-        System.out.println( aryLines[ i ] ) ;
-            }
-    }
-    
-    catch(IOException e){
-        System.out.println( e.getMessage() );
-        }   
-   
-    InputStream modelIn = new FileInputStream("en-sent.bin");
+        String file_name = "C:\\Users\\Patrick\\Documents\\Emma_Chap_I_Jane_Austen.txt";
 
-try {
-  SentenceModel model = new SentenceModel(modelIn);
-}
-catch (IOException e) {
-  e.printStackTrace();
-}
-finally {
-  if (modelIn != null) {
-    try {
-      modelIn.close();
+        InputStream modelIn = null;
+        
+        
+        //nur ab hier coden
+        try {
+            EmmaParser file = new EmmaParser(file_name);
+            String aryLines = file.OpenFile();
+
+            int i;
+            
+                System.out.println(aryLines);
+            
+            modelIn = new FileInputStream("en-sent.bin");
+            SentenceModel model = new SentenceModel(modelIn);
+            SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
+            String sentences[] = sentenceDetector.sentDetect(aryLines);
+
+            
+            for (i = 0; i < sentences.length; i++) {
+                System.out.println(sentences[i]);
+            }
+      //nur bis hier coden      
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+            
+            
+
+        } finally {
+
+            if (modelIn != null) {
+                try {
+                    modelIn.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
     }
-    catch (IOException e) {
-    }
-  }
-}
-    
-    }
-    
+
 }
--- src/emma/EmmaParser.java
+++ src/emma/EmmaParser.java
@@ -17,44 +17,45 @@ import opennlp.tools.sentdetect.SentenceModel;
  * @author Patrick
  */
 public class EmmaParser {
+
     public String path;
-    
-    public EmmaParser (String file_path) {
-            path = file_path;
+
+    public EmmaParser(String file_path) {
+        path = file_path;
     }
-    
-    public String[] OpenFile () throws IOException {
-    
-    FileReader fr = new FileReader (path);
-    BufferedReader textReader = new BufferedReader (fr);
-    
-    int numberOfLines = readLines( );
-    
-String[ ] textData = new String[numberOfLines];
 
-int i;
+    public String OpenFile() throws IOException {
 
-for (i=0; i < numberOfLines; i++) {
-textData[ i ] = textReader.readLine();
+        FileReader fr = new FileReader(path);
+        BufferedReader textReader = new BufferedReader(fr);
 
-}
-textReader.close( );
-return textData;
+        int numberOfLines = readLines();
+
+        String textData = "";
+
+        int i;
+
+        for (i = 0; i < numberOfLines; i++) {
+            textData += " " + textReader.readLine();
+
+        }
+        textReader.close();
+        return textData;
     }
-    
-    int readLines() throws IOException{
-    
-    FileReader file_to_read = new FileReader (path);
-    BufferedReader bf = new BufferedReader (file_to_read);
-    
-    String aLine;
-    
-   int numberOfLines =0;
-   
-   while ( ( aLine = bf.readLine( ) ) != null ) {
-        numberOfLines++;
-            }
-   bf.close();
-   return numberOfLines;
+
+    int readLines() throws IOException {
+
+        FileReader file_to_read = new FileReader(path);
+        BufferedReader bf = new BufferedReader(file_to_read);
+
+        String aLine;
+
+        int numberOfLines = 0;
+
+        while ((aLine = bf.readLine()) != null) {
+            numberOfLines++;
+        }
+        bf.close();
+        return numberOfLines;
     }
 }
